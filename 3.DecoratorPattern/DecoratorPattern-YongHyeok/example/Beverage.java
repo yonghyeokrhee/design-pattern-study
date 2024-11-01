1607e473734d96b38a0c82a0e11140f16104766f
yonghyeokrhee <joshuaisland@gmail.com>
2024-11-01 16:47:22 +0900

    예제 추가

--- 3.DecoratorPattern/DecoratorPattern-YongHyeok/example/Beverage.java
+++ 3.DecoratorPattern/DecoratorPattern-YongHyeok/example/Beverage.java
@@ -1,5 +1,34 @@
 public class Beverage {
+
+    double milkCost = 0.10;
+    double soyCost = 0.15;
+    double mochaCost = 0.20;
+    double whipCost = 0.10;
+
+
     public double cost(){
-        
+
+            double condimentCost = 0.0;
+            if (hasMilk()) {
+                condimentCost += milkCost;
+                              
+            }
+            if (hasSoy()){
+                condimentCost += soyCost;
+            }
+            if (hasMocha()){
+                condimentCost += mochaCost;
+            }
+            if (hasWhip()){
+                condimentCost += whipCost;
+            }
+
+            return condimentCost;
+        }
+
+    public boolean hasMilk(){return false;};
+    public boolean hasMocha(){return false;};
+    public boolean hasSoy(){return false;};
+    public boolean hasWhip(){return false;};
+
     }   
-}
